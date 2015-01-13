9fb5923e7fe66d916ca73fe2f39f8c535bd01a6f
Mark <markh4b@hotmail.com>
2015-01-13 17:13:50 +0100

    Added documentation for the round manager

--- Rules.ocd/ScenarioRounds.ocd/Script.c
+++ Rules.ocd/ScenarioRounds.ocd/Script.c
@@ -2,6 +2,27 @@
  This is a rule object that enables the game to run in rounds. That means after a goal is fulfilled, instead
  of finishing the scenario, the game starts merely a new round.
  {@section Script Architecture}
+ There are phases and game calls for rounds:@br
+ 1.) A game call is issued: {@c GameCallEx("OnRoundReset", round_number)}.
+     Objects should create a default state for a new round in this call, for example
+     players should be prevented from acting by putting them in spawn points.
+     Use {@link Rule_RoundManager#RegisterRoundStartBlocker} here if you want to prevent the round from
+     starting immediately.@br
+ 2.) Configuration Phase. This is completed once all round start blockers have removed themselves
+     with {@link Rule_RoundManager#RemoveRoundStartBlocker}.@br
+ 3.) A game call is issued: {@c GameCallEx("OnRoundStart", round_number)}.
+     This is the point where interactive objects should be enabled, for example spawn points
+     can spawn the players at this point.@br
+     Use {@link Rule_RoundManager#RegisterRoundEndBlocker} here if you want to prevent the round from
+     ending immediately.@br
+ 4.) Game phase. This is completed once all round end blockers have removed themselves
+     with {@link Rule_RoundManager#RemoveRoundEndBlocker}.@br
+ 5.) A game call is issued: {@c GameCallEx("OnRoundEnd", round_number)}. Remove undesired objects now.@br
+ 6.) Repeat from 1.)@br
+ @br
+ Note that a single {@link global#RoundManager RoundManager()} object will do one thing: Rush through the rounds quickly.
+ It actually needs rules or goals that register as blockers for the phases.
+ 
  @title Rounds
  @id index
  @author Marky
@@ -19,6 +40,27 @@ local Description = "$Description$";
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 // global functions
+
+
+/**
+ Use this to access the round manager object from other objects.
+ @note The function throws an error if it is called when no round manager exists.
+ @return object A {@link Rule_RoundManager#index round manager} object
+ @version 0.1.0
+ */
+global func RoundManager()
+{
+	var manager = FindObject(Find_ID(Rule_RoundManager));
+	
+	if (manager == nil)
+	{
+		FatalError(Format("A script relies on an existing %i object, but no such object exists", Rule_RoundManager));
+	}
+	else
+	{
+		return manager;
+	}
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -41,8 +83,9 @@ local chooser_id;		// def:  a chooser object that handles the configuration of t
 local chooser_player;	// int:  this player configures the round
 local max_rounds;		// int:  the game automatically stops after this many rounds
 
+//---------- engine calls ----------//
 
-protected func Construction()
+protected func Initialize()
 {
 	round_counter = 0; // on purpose
 	round_has_started = false;
@@ -69,34 +112,17 @@ public func Activate(int player_index)
 	return true;
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// non-functional and temporary stuff
+//---------- interaction with other objects ----------//
 
-global func RoundManager()
-{
-	var manager = FindObject(Find_ID(Rule_RoundManager));
-	
-	if (manager == nil)
-	{
-		FatalError(Format("A script relies on an existing %i object, but no such object exists", Rule_RoundManager));
-	}
-	else
-	{
-		return manager;
-	}
-}
-
-public func RegisterRoundEndBlocker(object blocker)
-{
-	CheckBlocker(blocker);
-
-	if (!IsValueInArray(round_end_blocker, blocker))
-	{
-		PushBack(round_end_blocker, blocker);
-	}
-}
-
+/**
+ Recognizes an object, so that the round does not start until
+ this object tells the round manager that it is ready.
+ @note The object has to tell the round manager that it is ready
+       by calling {@link Rule_RoundManager#RemoveRoundStartBlocker}.
+ @par blocker The object.
+ @related {@link Rule_RoundManager#RegisterRoundEndBlocker}
+ @version 0.1.0
+ */
 public func RegisterRoundStartBlocker(object blocker)
 {
 	CheckBlocker(blocker);
@@ -108,23 +134,70 @@ public func RegisterRoundStartBlocker(object blocker)
 }
 
 /**
- Ends the current round and prepares a new round.
- Issues the game call {@c OnRoundEnd(int round_number)}, increases the round counter and calls {@c OnRoundReset(int round_number)}.
+ Tells the round manager, that the object does not prevent the
+ round from starting anymore. The round starts if no object blocks
+ the round manager.
+ @par blocker The object.
+ @related {@link Rule_RoundManager#RegisterRoundStartBlocker}, {@link Rule_RoundManager#RemoveRoundEndBlocker}.
  @version 0.1.0
  */
-protected func NextRound()
+public func RemoveRoundStartBlocker(object blocker)
 {
-	// increase round number
-	round_counter++;
+	CheckBlocker(blocker);
+
+	RemoveArrayValue(round_start_blocker, blocker, false);
 	
-	GameCallEx("OnRoundReset", round_counter);
+	PrepareRoundStart();
 }
 
-protected func OnRoundEnd()
+/**
+ Recognizes an object, so that the round does not end until
+ this object tells the round manager that it is ready.
+ @note The object has to tell the round manager that it is ready
+       by calling {@link Rule_RoundManager#RemoveRoundEndBlocker}.
+ @par blocker The object.
+ @related {@link Rule_RoundManager#RegisterRoundStartBlocker}
+ @version 0.1.0
+ */
+public func RegisterRoundEndBlocker(object blocker)
 {
-	// TODO: wait until no object with "BlockRoundEnd" returns true
-	// then reset  the round
+	CheckBlocker(blocker);
+
+	if (!IsValueInArray(round_end_blocker, blocker))
+	{
+		PushBack(round_end_blocker, blocker);
+	}
 }
+
+/**
+ Tells the round manager, that the object does not prevent the
+ round from ending anymore. The round ends if no object blocks
+ the round manager.
+ @par blocker The object.
+ @related {@link Rule_RoundManager#RegisterRoundEndBlocker}, {@link Rule_RoundManager#RemoveRoundStartBlocker}.
+ @version 0.1.0
+ */
+public func RemoveRoundEndBlocker(object blocker)
+{
+	CheckBlocker(blocker);
+
+	RemoveArrayValue(round_end_blocker, blocker, false);
+	
+	PrepareRoundEnd();
+}
+
+/**
+ Use this to check if a round in the game is currently active.
+ @return {@c true} if the round has started, including the game call "OnRoundStart",@br
+         {@c false} if the round has ended, including the game call "OnRoundend"
+ @version 0.1.0
+ */
+public func IsRoundActive()
+{
+	return round_has_started;
+}
+
+//---------- rection to game calls ----------//
 
 /**
  GameCall when the round is reset. 
@@ -132,26 +205,6 @@ protected func OnRoundEnd()
  */
 protected func OnRoundReset(int round_number)
 {
-/*
-	if (chooser_id == nil)
-	{
-		// nothing to do yet, just start the new round
-		GameCallEx("OnRoundStart", round_counter);
-	}
-	else
-	{
-		// has a chooser? Configure the settings!
-		if (cycle_chooser)
-		{
-			chooser_player = (round_counter - 1) % GetPlayerCount(C4PT_User);
-		}
-		
-		// TODO
-		
-		// nothing to do yet, just start the new round
-		GameCallEx("OnRoundStart", round_counter);
-	}
-*/
 	if (chooser_id != nil)
 	{
 		// has a chooser? Configure the settings!
@@ -164,7 +217,40 @@ protected func OnRoundReset(int round_number)
 	ScheduleCall(this, "PrepareRoundStart", 5, 0);
 }
 
-public func PrepareRoundStart()
+
+//---------- internal calls ----------//
+
+/**
+ Checks if the object actually exists and throws an error if not.
+ @par blocker The blocker object.
+ @ignore
+ */
+private func CheckBlocker(object blocker)
+{
+	if (blocker == nil)
+	{
+		FatalError(Format("Must specify an existing object! Parameter is %v", blocker));
+	}
+}
+
+/**
+ Prepares a new round.
+ Issues the game call {@c OnRoundEnd(int round_number)}, increases the round counter and calls {@c OnRoundReset(int round_number)}.
+ @version 0.1.0
+ */
+private func NextRound()
+{
+	// increase round number
+	round_counter++;
+	
+	GameCallEx("OnRoundReset", round_counter);
+}
+
+/**
+ Gets called every time a round start blocker is removed.
+ @version 0.1.0
+ */
+private func PrepareRoundStart()
 {
 	if (GetLength(round_start_blocker) == 0)
 	{
@@ -172,7 +258,11 @@ public func PrepareRoundStart()
 	}
 }
 
-public func PrepareRoundEnd()
+/**
+ Gets called every time a round end blocker is removed.
+ @version 0.1.0
+ */
+private func PrepareRoundEnd()
 {
 	if (GetLength(round_end_blocker) == 0)
 	{
@@ -181,6 +271,10 @@ public func PrepareRoundEnd()
 	}
 }
 
+/**
+ Issues the game call {@c GameCallEx("OnRoundEnd", round_number)}.
+ @version 0.1.0
+ */
 private func DoRoundEnd()
 {
 	// reset the activity status
@@ -188,6 +282,10 @@ private func DoRoundEnd()
 	GameCallEx("OnRoundEnd", round_counter);
 }
 
+/**
+ Issues the game call {@c GameCallEx("OnRoundStart", round_number)}.
+ @version 0.1.0
+ */
 private func DoRoundStart()
 {
 	round_has_started = true;
@@ -196,30 +294,6 @@ private func DoRoundStart()
 	ScheduleCall(this, "PrepareRoundEnd", 5, 0);
 }
 
-
-
-public func RemoveRoundEndBlocker(object blocker)
-{
-	CheckBlocker(blocker);
-
-	RemoveArrayValue(round_end_blocker, blocker, false);
-	
-	PrepareRoundEnd();
-}
-
-public func RemoveRoundStartBlocker(object blocker)
-{
-	CheckBlocker(blocker);
-
-	RemoveArrayValue(round_start_blocker, blocker, false);
-	
-	PrepareRoundStart();
-}
-
-private func CheckBlocker(object blocker)
-{
-	if (blocker == nil)
-	{
-		FatalError(Format("Must specify an existing object! Parameter is %v", blocker));
-	}
-}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// non-functional and temporary stuff
