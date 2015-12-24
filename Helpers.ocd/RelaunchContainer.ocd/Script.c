93e97aa403ec3b91cf3eb7538950d71a3a333c3f
Mark <markh4b@hotmail.com>
2015-12-24 16:21:21 +0100

    Bugfix: Relaunch Container
    The relaunch container relied on a local change in the orignal OC objects. The bug is fixed by not including the original container and implementing the code in this object again. This may not be a good practice, but it makes the object less prone to accidental changes.
    
    (cherry picked from commit 058c2297aeaf7b2e6b810c55677ac9161ba1a4be)

--- Helpers.ocd/RelaunchContainer.ocd/Script.c
+++ Helpers.ocd/RelaunchContainer.ocd/Script.c
@@ -13,10 +13,155 @@
  @version 0.1.0
 */
 
-#include RelaunchContainer
+
 
 static const RELAUNCH_Default_Time = 360; // hold 10 seconds
 static const RELAUNCH_Default_Hold = true;
+static const RELAUNCH_Factor_Second = 36;
+
+//----------------------------------------------------------------------------
+/*--
+	Relaunch container
+	Author: Maikel
+
+	This container holds the clonk after relaunches.
+	* The time the clonk is held can be specified with SetRelaunchTime(int time);
+	* After that time the clonk is released and OnClonkLeftRelaunch(object clonk) is called in the scenario script.
+	* Optionally the clonk can choose a weapon if GetRelaunchWeaponList in the scenario script returns a valid id-array.
+--*/
+
+local time;
+local menu;
+local hold;
+local has_selected;
+
+local crew;
+
+/**
+ Sets the time, in seconds, the crew is held in the container.
+ @par to_time The time to hold the crew, in seconds.
+ @par to_hold If set to true, then the crew will be contained in
+              the container until the time has fully run out.
+              Otherwise the player can exit the container earlier.
+ @author Maikel
+ @version 0.2.1
+ */
+public func SetRelaunchTime(int to_time, bool to_hold)
+{
+	time = to_time * RELAUNCH_Factor_Second;
+	hold = to_hold;
+}
+
+/**
+ Returns the time, in seconds the clonk is held.
+ @author Maikel
+ @version 0.2.1
+ */
+public func GetRelaunchTime() { return time / RELAUNCH_Factor_Second; }
+
+/**
+ Retrieve weapon list from scenario.
+ This issues a game call "RelaunchWeaponList" that should return an array of IDs.
+ @author Maikel
+ @version 0.2.1
+ */ 
+public func WeaponList() { return GameCall("RelaunchWeaponList"); }
+
+
+private func OpenWeaponMenu(object clonk)
+{
+	if (!clonk)	return;	
+	if (!menu)
+	{
+		var weapons = WeaponList();
+		if (weapons && GetLength(weapons) > 0)
+		{
+			menu = CreateObject(MenuStyle_Default, nil, nil, clonk->GetOwner());
+			menu->SetPermanent();
+			menu->SetTitle(Format("$MsgWeapon$", time / RELAUNCH_Factor_Second));
+			clonk->SetMenu(menu); 
+
+			for (var weapon in weapons)
+				menu->AddItem(weapon, weapon->GetName(), nil, this, "OnWeaponSelected", weapon);
+	
+			menu->Open();
+		}
+	}
+}
+
+func FxIntTimeLimitTimer(object target, effect, int fxtime)
+{
+	if (!crew)
+	{
+		RemoveObject();
+		return FX_Execute_Kill;
+	}
+	if (fxtime >= time)
+	{
+		var weapons = WeaponList();
+		if (!has_selected && weapons && GetLength(weapons) > 0)
+			GiveWeapon(weapons[Random(GetLength(weapons))]);
+		RelaunchCrew();
+		return FX_Execute_Kill;
+	}
+	if (menu)
+		menu->SetTitle(Format("$MsgWeapon$", (time - fxtime) / RELAUNCH_Factor_Second));
+	else
+		PlayerMessage(crew->GetOwner(), Format("$MsgRelaunch$", (time - fxtime) / RELAUNCH_Factor_Second));
+	return FX_OK;
+}
+
+public func OnWeaponSelected(id weapon)
+{
+	GiveWeapon(weapon);
+	
+	has_selected = true;
+	// Close menu manually, to prevent selecting more weapons.
+	if (menu)
+		menu->Close();
+
+	if (!hold)
+		RelaunchCrew();
+	return true;
+}
+
+private func RelaunchCrew()
+{
+	crew->Exit();
+	if (menu)
+		menu->Close();
+	PlayerMessage(crew->GetOwner(), "");
+	OnRelaunchCrew(crew);
+	RemoveObject();
+}
+
+
+/**
+ Callback when the contained crew is relaunched.
+ This happens after the crew has left the container and
+ before the container is removed. 
+ @version 0.2.0
+ */
+public func OnRelaunchCrew(object crew)
+{
+}
+
+/**
+ Is called when the player selects a weapon.
+ This method should create the weapon and
+ let the player collect it. By default this function
+ does nothing.
+ @par weapon_id This weapon was selected.
+ */
+private func GiveWeapon(id weapon_id)
+{
+}
+
+public func SaveScenarioObject() { return false; }
+
+local Name = "$Name$";
+
+//----------------------------------------------------------------------------
 
 protected func Initialize()
 {
@@ -45,8 +190,8 @@ public func StartRelaunch(object clonk)
 {
 	if (PrepareRelaunch(clonk))
 	{
-		ScheduleCall(this, "OpenWeaponMenu", 36, 0, clonk);
-		AddEffect("IntTimeLimit", this, 100, 36, this);
+		ScheduleCall(this, "OpenWeaponMenu", RELAUNCH_Factor_Second, 0, clonk);
+		AddEffect("IntTimeLimit", this, 100, RELAUNCH_Factor_Second, this);
 		return true;
 	}
 	else
