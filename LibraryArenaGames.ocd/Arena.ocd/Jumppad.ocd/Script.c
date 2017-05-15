7b273b2fcd28e891d560213c442acc552fb96219
Mark <markh4b@hotmail.com>
2017-05-15 22:20:05 +0200

    JumpPad: Rename variables

--- LibraryArenaGames.ocd/Arena.ocd/Jumppad.ocd/Script.c
+++ LibraryArenaGames.ocd/Arena.ocd/Jumppad.ocd/Script.c
@@ -5,11 +5,11 @@ static const JUMPPAD_LAYER_LIGHT = 2;
 static const JUMPPAD_LAYER_GLOW = 3;
 static const JUMPPAD_LAYER_BASE = 4;
 
-local strength;
-local color;
+local pad_strength;
+local pad_color;
+local pad_size;
 local base_r;
-local active;
-local size;
+local is_active;
 
 
 func Initialize()
@@ -59,7 +59,7 @@ func SetBaseGraphics(string name, id type)
 
 func SetSize(int size)
 {
-	this.size = size;
+	pad_size = size;
 	DrawBase();
 	DrawPad();
 	return this;
@@ -68,14 +68,14 @@ func SetSize(int size)
 
 func DrawPad()
 {
-	SetObjDrawTransform(size, nil, nil, nil, size, nil, 0);
- 	SetObjDrawTransform(1000, 0, 0, 0, 1000, -4 * size, JUMPPAD_LAYER_GLOW);
+	SetObjDrawTransform(pad_size, nil, nil, nil, pad_size, nil, 0);
+ 	SetObjDrawTransform(1000, 0, 0, 0, 1000, -4 * pad_size, JUMPPAD_LAYER_GLOW);
 }
 
 func DrawBase()
 {
 
-	var fsin = +Sin(base_r, size), fcos = Cos(base_r, size);
+	var fsin = +Sin(base_r, pad_size), fcos = Cos(base_r, pad_size);
 
 	var width  = +fcos;
 	var height = +fcos;
@@ -95,17 +95,17 @@ func DrawBase()
 
 func Activate()
 {
-	active = true;
+	is_active = true;
 	SetClrModulation(0xffffff, JUMPPAD_LAYER_LAMP);
-	SetClrModulation(color, JUMPPAD_LAYER_LIGHT);
-	SetClrModulation(color, JUMPPAD_LAYER_GLOW);
+	SetClrModulation(pad_color, JUMPPAD_LAYER_LIGHT);
+	SetClrModulation(pad_color, JUMPPAD_LAYER_GLOW);
 }
 
 
 func Deactivate()
 {
-	active = false;
-	SetClrModulation(SetRGBaValue(color, 120, RGBA_ALPHA), JUMPPAD_LAYER_LAMP);
+	is_active = false;
+	SetClrModulation(SetRGBaValue(pad_color, 120, RGBA_ALPHA), JUMPPAD_LAYER_LAMP);
 	SetClrModulation(RGBa(255, 255, 255, 1), JUMPPAD_LAYER_LIGHT);
 	SetClrModulation(RGBa(255, 255, 255, 1), JUMPPAD_LAYER_GLOW);
 }
@@ -113,7 +113,7 @@ func Deactivate()
 
 func SetEffectColor(int color)
 {
-	this.color = color;
+	this.pad_color = color;
 	Activate();
 	return this;
 }
@@ -121,18 +121,18 @@ func SetEffectColor(int color)
 
 func SetStrength(int strength)
 {
-	this.strength = strength;
+	this.pad_strength = strength;
 	return this;
 }
 
 func GetStrength()
 {
-	return this.strength;
+	return this.pad_strength;
 }
 
 func CheckBounce()
 {
-	if (active)
+	if (is_active)
 	{
 		// bounce targets
 		for (var target in GetBounceTargets())
@@ -161,8 +161,8 @@ func Bounce(object target)
 	var xdir_new, ydir_new, xdir_old, ydir_old;
 	xdir_old = target->GetXDir();
 	ydir_old = target->GetYDir();
-	xdir_new = +Sin(GetR(), strength);
-	ydir_new = -Cos(GetR(), strength);
+	xdir_new = +Sin(GetR(), pad_strength);
+	ydir_new = -Cos(GetR(), pad_strength);
 	// the object keeps a bit of its old velocity
 	xdir_new += xdir_old / 10;
 	ydir_new += ydir_old / 10;
@@ -180,14 +180,14 @@ func OnBounce(object target)
 
 func ParticleEffect()
 {
-	if (active)
+	if (is_active)
 	{
 		// particle effect for jump pad "waves"
 		var lifetime = 40;
-		var range = 180 * size / 1000; // 18 pixels
+		var range = 180 * pad_size / 1000; // 18 pixels
 		var xdir = +Sin(GetR(), range / lifetime);
 		var ydir = -Cos(GetR(), range / lifetime);
-		CreateParticle("Arena_JumpPad", 0, 0, xdir, ydir, lifetime, Particles_JumpPad(GetR(), color, size), 1);
+		CreateParticle("Arena_JumpPad", 0, 0, xdir, ydir, lifetime, Particles_JumpPad(GetR(), pad_color, pad_size), 1);
 	}
 }
 
@@ -201,9 +201,9 @@ func Particles_JumpPad(int angle, int particle_color, int particle_size)
 	{
 		Size = 3 * particle_size / 1000,
 		Alpha = PV_KeyFrames(0, 0, 0, 400, 255, 1000, 0),
-		R = GetRGBaValue(color, RGBA_RED),
-		G = GetRGBaValue(color, RGBA_GREEN),
-		B = GetRGBaValue(color, RGBA_BLUE),
+		R = GetRGBaValue(pad_color, RGBA_RED),
+		G = GetRGBaValue(pad_color, RGBA_GREEN),
+		B = GetRGBaValue(pad_color, RGBA_BLUE),
 		Rotation = angle,
 	};
 }
@@ -225,9 +225,9 @@ func SaveScenarioObject(proplist props)
 		props->AddCall("SetBaseR", this, "SetBaseR", base_r);
 	}
 
-	if (strength)
+	if (pad_strength)
 	{
-		props->AddCall("SetStrength", this, "SetStrength", strength);
+		props->AddCall("SetStrength", this, "SetStrength", pad_strength);
 	}
 	
 	return true;
