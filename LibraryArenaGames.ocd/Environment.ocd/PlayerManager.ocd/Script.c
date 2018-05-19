cf8a9cec130e06b176359632aa9ca7e22e36107b
Mark <markh4b@hotmail.com>
2018-05-20 00:37:34 +0200

    Added option for saving stats.

--- LibraryArenaGames.ocd/Environment.ocd/PlayerManager.ocd/Script.c
+++ LibraryArenaGames.ocd/Environment.ocd/PlayerManager.ocd/Script.c
@@ -16,6 +16,7 @@ local Description = "$Description$";
 local Singleton_GetterCreatesInstance = true;
 
 local player_elimination;
+local player_stats;
 
 /* --- Engine callbacks --- */
 
@@ -24,6 +25,7 @@ func Construction (object creator)
 	_inherited(creator, ...);
 	
 	player_elimination = [];
+	player_stats = [];
 }
 
 func OnGameOver()
@@ -49,9 +51,80 @@ func OnGameOver()
 	@par player The player number. Is saved by player ID internally.
 	@par eliminate Default is {@code true}. Pass {@code false} if you want to
 	               revoke the setting.
+	               
+	@return object The instance itself for further function calls.
  */
 public func MarkForElimination(int player, bool eliminate)
 {
 	player_elimination[GetPlayerID(player)] = eliminate ?? true;
 	return this;
+}
+
+
+/**
+	Defines a statistic for a player.
+	
+	@par player The player number. Is saved by player ID internally.
+	@par stat The statistics name.
+	               
+	@return object The instance itself for further function calls.
+ */
+public func SetPlayerStat(int player, string stat, value)
+{
+	AssertNotNil(stat);
+	
+	var index = GetPlayerID(player);
+	if (!player_stats[index])
+	{
+		player_stats[index] = {};
+	}
+	
+	if (player_stats[index][stat])
+	{
+		if (value == nil)
+		{
+			player_stats[index][stat] = nil;
+		}
+		else if (player_stats[index][stat].Type == GetType(value))
+		{
+			player_stats[index][stat].Value = value;
+		}
+		else
+		{
+			FatalError(Format("Type mismatch: Trying to set %v, expected %v", GetType(value), player_stats[index][stat].Type));
+		}
+	}
+	else
+	{
+		player_stats[index][stat] = { Type = GetType(value), Value = value};
+	}
+	return this;
+}
+
+
+/**
+	Reads a statistic for a player.
+	
+	@par player The player number. Is saved by player ID internally.
+	               
+	@return The value.
+ */
+public func GetPlayerStat(int player, string stat)
+{
+	AssertNotNil(stat);
+	
+	var index = GetPlayerID(player);
+	if (!player_stats[index])
+	{
+		player_stats[index] = {};
+	}
+	
+	if (player_stats[index][stat])
+	{
+		return player_stats[index][stat].Value;
+	}
+	else
+	{
+		return nil;
+	}
 }
